3d7aca0170f99e13c640cc98294611813e6a48cc
Michael Droz <michael.droz@gmail.com>
2014-03-13 16:57:47 +0000

    Added before_action to check for apiGEE validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
-end
+  before_action :validate_apigee
+ 
+  private
+ 
+  def validate_apigee
+    if params[:apigee_key] != ENV["APIGEE"]
+      render json: status: "Unauthorized" # halts request cycle
+    end
+  end
+end 
