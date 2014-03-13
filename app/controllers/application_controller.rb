df31e7506105571ac7a5a4022a41747d06bb0620
Michael Droz <michael.droz@gmail.com>
2014-03-13 18:41:45 +0000

    Trying http_basic_auth again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::API
-  
-  #before_action :validate_apigee
-  #before_action :basic_auth
     http_basic_authenticate_with name: "apigee", password: "ColdWar44"
-=begin
-  def validate_apigee
-    if params[:apigee_key] != ENV[APIGEE]
-      render json: status: "Unauthorized" # halts request cycle
-    else
-      return
-    end
-  end
-=end
 end 
