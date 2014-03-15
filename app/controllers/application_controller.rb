17c47999fbeefdfef3d62addc4839cb25d9914ea
Michael Droz <michael.droz@gmail.com>
2014-03-15 01:12:30 +0000

    Trying unix env variables again...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API 
 include  ActionController::HttpAuthentication::Basic::ControllerMethods
-   http_basic_authenticate_with name: <%=ENV["BASE_AUTH_NAME"] %>, password: <%=ENV["BASE_AUTH_PASS"] %>
+   http_basic_authenticate_with name: ENV["BASE_AUTH_NAME"], password: ENV["BASE_AUTH_PASS"]
 end 
