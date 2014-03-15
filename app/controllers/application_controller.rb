49231642c72593ad7e92995c436b4a7bcb1b8375
Michael Droz <michael.droz@gmail.com>
2014-03-15 00:50:50 +0000

    Fixed include for HttpAuthenticate module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
-class ApplicationController < ActionController::API < ActionController::HttpAuthentication::Basic::ControllerMethods
+class ApplicationController < ActionController::API 
+include  ActionController::HttpAuthentication::Basic::ControllerMethods
    http_basic_authenticate_with name: "apigee", password: "ColdWar44"
 end 
