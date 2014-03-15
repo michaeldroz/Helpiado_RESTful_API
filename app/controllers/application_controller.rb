7c51d19f66268df00a1b9a52ffb80197d173a751
Michael Droz <michael.droz@gmail.com>
2014-03-15 00:42:35 +0000

    Added modules to support http_basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API < ActionController::HttpAuthentication::Basic::ControllerMethods
    http_basic_authenticate_with name: "apigee", password: "ColdWar44"
 end 
