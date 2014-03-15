f06cfece1568d1b3e471698ce19ece49d0c47a0e
Michael Droz <michael.droz@gmail.com>
2014-03-15 01:08:51 +0000

    created unix env variables for http_basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API 
 include  ActionController::HttpAuthentication::Basic::ControllerMethods
-   http_basic_authenticate_with name: "apigee", password: "ColdWar44"
+   http_basic_authenticate_with name: <%=ENV["BASE_AUTH_NAME"] %>, password: <%=ENV["BASE_AUTH_PASS"] %>
 end 
