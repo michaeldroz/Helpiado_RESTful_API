54e0de6ec8924af698342a6cf6ba4bffb6156e1b
Michael Droz <michael.droz@gmail.com>
2014-03-19 04:47:14 +0000

    Trying out Sergey's changes.

--- app/controllers/people_controller_archive.rb
+++ app/controllers/people_controller_archive.rb
@@ -1,26 +1,24 @@
 class PeopleController < ApplicationController
-  before_action :set_person, only: [:show, :edit, :update, :destroy]
-
   # GET /people
   # GET /people.json
   def index
     @people = Person.all
+
+    render json: @people
   end
 
   # GET /people/1
   # GET /people/1.json
   def show
+    @person = Person.find(params[:id])
+
     render json: @person
   end
 
   # POST /people
   # POST /people.json
-  def new
-     @person = Person.new
-  end  
-  
- def create
-    @person = Person.new(person_params.merge(:vote_balance => 50)) #hard coding initial vote balance. 
+  def create
+    @person = Person.create(person_params.merge(:vote_balance => 50)) #hard coding initial vote balance. 
     if @person.save
       render json: @person, status: :created, location: @person
     else
@@ -31,27 +29,25 @@ class PeopleController < ApplicationController
   # PATCH/PUT /people/1
   # PATCH/PUT /people/1.json
   def update
+    @person = Person.find(params[:id])
+
     if @person.update(person_params)
-        head :no_content
-      else
-         render json: @person.errors, status: :unprocessable_entity
-      end
+      head :no_content
+    else
+      render json: @person.errors, status: :unprocessable_entity
     end
+  end
 
   # DELETE /people/1
   # DELETE /people/1.json
   def destroy
-    @person.destroy 
+    @person = Person.find(params[:id])
+    @person.destroy
 
     head :no_content
   end
 
 private
-# Use callbacks to share common setup or constraints between actions 
-
-def set_person
-    @person = Person.find(params[:id])
-end
 def person_params
 	params.require(:person).permit(:id, :first_name, :last_name, :email_address, :password, :vote_balance)
 end
