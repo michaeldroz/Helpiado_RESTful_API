37fb2ae6b5028b6ac4a58534d31594d9dac0d755
Michael Droz <michael.droz@gmail.com>
2014-03-22 01:17:55 +0000

    routing for vote_balances

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Helpiado::Application.routes.draw do
   resources :people, except: [:new, :edit]
   resources :deeds, except: [:new, :edit]
-  resources :vote_balances: except: [:new, :edit]  
+  resources :vote_balances, except: [:new, :edit]  
 # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
