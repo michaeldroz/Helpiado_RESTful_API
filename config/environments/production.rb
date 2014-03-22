d79522736636741c230f596e61c5d377e165715e
Michael Droz <michael.droz@gmail.com>
2014-03-22 02:41:25 +0000

    set logs to debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Helpiado::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
