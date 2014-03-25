4968bd36d4700a6f796e39ba7ef9bf3048bb9e55
Michael Droz <michael.droz@gmail.com>
2014-03-25 20:48:42 +0000

    Def for person_params_with_vote_balance

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,6 @@
 class PeopleController < ApplicationController
   #Tomorrow to do: Design vote_balance models
-  before_action :set_person, only: [:show, :edit, :update, :destroy]  
-  @person_params_with_vote_balance = person_params.merge(vote_balance.attributes) 
+  before_action :set_person, only: [:show, :edit, :update, :destroy]   
   # GET /people
   # GET /people.json
   # How to merge vote_balance for each..probably a do |x| .. 
@@ -86,4 +85,9 @@ def person_params
 	#params.require(:person).permit(:id, :first_name, :last_name, :email_address, :password)
         params.permit(:id, :first_name, :last_name, :email_address, :password)
 end
+
+def person_params_with_vote_balance
+    person_params.merge(vote_balance_attributes: )
+end 
+
 end 
