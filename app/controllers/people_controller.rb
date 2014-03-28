8ec99bbdd69e19c5b30d8608fdcb8b9b8951b37b
Michael Droz <michael.droz@gmail.com>
2014-03-28 03:34:19 +0000

    .permit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -87,7 +87,7 @@ end
 #        params.permit(:id, :first_name, :last_name, :email_address, :password)
 #end 
   def person_params_with_vote_balance
-    params.permit(:id, :first_name, :last_name, :email_address, :password, :vote_balance)
-     params.merge(vote_balances_attributes: {vote_balance: 50})
+    params.permit(:id, :first_name, :last_name, :email_address, :password)
+    params.merge(vote_balances_attributes: {vote_balance: 50}).permit(:vote_balance)
   end
 end 
