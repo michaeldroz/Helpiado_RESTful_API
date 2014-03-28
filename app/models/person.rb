f54df81940cb0e2395d145b14e42b79036a4e88d
Michael Droz <michael.droz@gmail.com>
2014-03-28 02:26:31 +0000

    typo

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
     has_many :deeds, dependent: :destroy
     has_one :vote_balances, dependent: :destroy
-    accepts_nested_attributes_for :vote_balance
+    accepts_nested_attributes_for :vote_balances
   
     #example transaction code
     #person.transaction do 
