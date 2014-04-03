99998fefdf8c79e0a9941adc9d33a27e313ee7bf
Michael Droz <michael.droz@gmail.com>
2014-04-03 02:32:01 +0000

    Added group to calculate method

--- app/controllers/deeds_controller.rb
+++ app/controllers/deeds_controller.rb
@@ -11,8 +11,8 @@ class DeedsController < ApplicationController
   # GET /deeds/1.json
   def show
     @deed = Deed.find(params[:id])
-    @deed_score = DeedVote.sum(:votes, :conditions =>['deed_id = ?', params[:id]])
-    render :json => @deed.attributes.merge(@deed_score)
+    @deed_score = DeedVote.group('votes').sum(:votes, :conditions =>['deed_id = ?', params[:id]])
+    render :json => @deed.attributes.merge(@deed_score.attributes)
     #render json: @deed
   end
 
